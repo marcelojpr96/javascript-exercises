7ec990dcba27510032d56eb6419baf632ecaed50
marcelojpr96 <marcelojpr96@gmail.com>
2025-12-09 07:35:34 +0000

    07 done

---
+++ 07_reverseString done/reverseString.js
@@ -0,0 +1,7 @@
+const reverseString = function(string_to_reverse) {
+    console.log(string_to_reverse.split("").reverse().join(""))
+    return string_to_reverse.split("").reverse().join("");
+};
+
+// Do not edit below this line
+module.exports = reverseString;
